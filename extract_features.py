490475a163264572f41e0f862edf66f7c10fc024
hanhxiao <hanhxiao@tencent.com>
2018-11-08 17:39:42 +0800

    fix runconfig

--- extract_features.py
+++ extract_features.py
@@ -13,7 +13,6 @@ import re
 import tensorflow as tf
 from tensorflow.python.estimator.estimator import Estimator
 from tensorflow.python.estimator.model_fn import EstimatorSpec
-from tensorflow.python.estimator.run_config import RunConfig
 
 import modeling
 import tokenization
@@ -23,50 +22,30 @@ flags = tf.flags
 FLAGS = flags.FLAGS
 
 flags.DEFINE_string("input_file", None, "")
-
 flags.DEFINE_string("output_file", None, "")
-
 flags.DEFINE_string("layers", "-1,-2,-3,-4", "")
-
-flags.DEFINE_string(
-    "bert_config_file", None,
-    "The config json file corresponding to the pre-trained BERT model. "
-    "This specifies the model architecture.")
-
-flags.DEFINE_integer(
-    "max_seq_length", 128,
-    "The maximum total input sequence length after WordPiece tokenization. "
-    "Sequences longer than this will be truncated, and sequences shorter "
-    "than this will be padded.")
-
-flags.DEFINE_string(
-    "init_checkpoint", None,
-    "Initial checkpoint (usually from a pre-trained BERT model).")
-
+flags.DEFINE_string("bert_config_file", None,
+                    "The config json file corresponding to the pre-trained BERT model. "
+                    "This specifies the model architecture.")
+flags.DEFINE_integer("max_seq_length", 128,
+                     "The maximum total input sequence length after WordPiece tokenization. "
+                     "Sequences longer than this will be truncated, and sequences shorter "
+                     "than this will be padded.")
+flags.DEFINE_string("init_checkpoint", None,
+                    "Initial checkpoint (usually from a pre-trained BERT model).")
 flags.DEFINE_string("vocab_file", None,
                     "The vocabulary file that the BERT model was trained on.")
-
-flags.DEFINE_bool(
-    "do_lower_case", True,
-    "Whether to lower case the input text. Should be True for uncased "
-    "models and False for cased models.")
-
+flags.DEFINE_bool("do_lower_case", True,
+                  "Whether to lower case the input text. Should be True for uncased "
+                  "models and False for cased models.")
 flags.DEFINE_integer("batch_size", 32, "Batch size for predictions.")
-
 flags.DEFINE_bool("use_tpu", False, "Whether to use TPU or GPU/CPU.")
-
 flags.DEFINE_string("master", None,
                     "If using a TPU, the address of the master.")
-
-flags.DEFINE_integer(
-    "num_tpu_cores", 8,
-    "Only used if `use_tpu` is True. Total number of TPU cores to use.")
-
-flags.DEFINE_bool(
-    "use_one_hot_embeddings", False,
-    "If True, tf.one_hot will be used for embedding lookups, otherwise "
-    "tf.nn.embedding_lookup will be used. On TPUs, this should be True "
-    "since it is much faster.")
+flags.DEFINE_bool("use_one_hot_embeddings", False,
+                  "If True, tf.one_hot will be used for embedding lookups, otherwise "
+                  "tf.nn.embedding_lookup will be used. On TPUs, this should be True "
+                  "since it is much faster.")
 
 
 class InputExample(object):
@@ -136,7 +115,7 @@ def input_fn_builder(features, seq_length):
     return input_fn
 
 
-def model_fn_builder(bert_config, init_checkpoint, layer_indexes, use_tpu,
+def model_fn_builder(bert_config, init_checkpoint, layer_indexes,
                      use_one_hot_embeddings):
     """Returns `model_fn` closure for TPUEstimator."""
 
@@ -330,8 +309,6 @@ def main(_):
     tokenizer = tokenization.FullTokenizer(
         vocab_file=FLAGS.vocab_file, do_lower_case=FLAGS.do_lower_case)
 
-    run_config = RunConfig()
-
     examples = read_examples(FLAGS.input_file)
 
     features = convert_examples_to_features(
@@ -341,14 +318,15 @@ def main(_):
     for feature in features:
         unique_id_to_feature[feature.unique_id] = feature
 
+    print(features)
+    exit()
     model_fn = model_fn_builder(
         bert_config=bert_config,
         init_checkpoint=FLAGS.init_checkpoint,
         layer_indexes=layer_indexes,
-        use_tpu=FLAGS.use_tpu,
         use_one_hot_embeddings=FLAGS.use_one_hot_embeddings)
 
-    estimator = Estimator(model_fn=model_fn, config=run_config)
+    estimator = Estimator(model_fn=model_fn)
 
     input_fn = input_fn_builder(
         features=features, seq_length=FLAGS.max_seq_length)
