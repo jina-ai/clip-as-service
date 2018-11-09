a3e3711f0afee299332c3619977cd931e4a69a9c
hanhxiao <hanhxiao@tencent.com>
2018-11-09 18:50:43 +0800

    update client

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ except:
 from utils.server import ServerTask
 
 if __name__ == '__main__':
-    server = ServerTask(sys.argv[1])
+    server = ServerTask(sys.argv[1], max_seq_len=50)
     server.start()
     server.join()
