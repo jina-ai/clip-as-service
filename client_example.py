044bd4bc62919e76c6567bd5d5a31f299072f71b
hanhxiao <hanhxiao@tencent.com>
2018-11-12 18:39:35 +0800

    refactor bc

--- client_example.py
+++ client_example.py
@@ -3,7 +3,7 @@ import time
 from service.client import BertClient
 
 if __name__ == '__main__':
-    ec = BertClient(ip='localhost', port=5555)
+    bc = BertClient(ip='localhost', port=5555)
     # encode a list of strings
     with open('README.md', encoding='utf8') as fp:
         data = [v for v in fp if v.strip()]
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     for j in range(1, 200, 10):
         start_t = time.time()
         tmp = data * j
-        ec.encode(tmp)
+        bc.encode(tmp)
         time_t = time.time() - start_t
         print('encoding %d strs in %.2fs, speed: %d/s' %
               (len(tmp), time_t, int(len(tmp) / time_t)))
