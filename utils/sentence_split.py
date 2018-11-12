3618f2773407f7c6016940a6dca8c3e12565f6d8
hanhxiao <hanhxiao@tencent.com>
2018-11-12 10:46:39 +0800

    add lb

--- utils/sentence_split.py
+++ utils/sentence_split.py
@@ -3,35 +3,31 @@ import re
 
 
 class SentenceSplitter:
-    def __init__(self, min_len=5, max_len=20):
+    def __init__(self, min_len=2, max_len=20):
         self.min_len = min_len
         self.max_len = max_len
-        self.must_split = r"[^.。！？!?]*[.。！？!?]"
-        self.maybe_split = r"[^,，、.。:：;；(（)）]*[,，、.。:：;；(（)）]"
-        self.html_symbol = {
-            '&gt', '&nbsp'
-        }
+        self.must_split = r"[.。！？!?]+"
+        self.maybe_split = r"[,，、.。:：;；(（)）\s]+"
 
     def _is_ascii(self, s):
         return len(s) == len(s.encode())
 
     def _get_printable(self, p):
-        result = html.unescape(p)
-        result = ''.join(c for c in result if c.isprintable())
-        return result
+        p = html.unescape(p)  # decode html entity
+        p = ''.join(c for c in p if c.isprintable())  # remove unprintable char
+        p = ''.join(p.split())  # remove space
+        return p
 
     def _check_p(self, p):
         if (len(p) > self.min_len and  # must longer
                 not self._is_ascii(p) and  # must not all english
-                len(re.findall('\s', p)) == 0 and  # must not contain spaces -> likely spam
+                # len(re.findall('\s', p)) == 0 and  # must not contain spaces -> likely spam
                 '\\x' not in p):  # must not contain bad unicode char
             return True
 
     def _split(self, p, reg):
-        matches = re.finditer(reg, self._get_printable(p.strip()))
-        sent = [match.group() for matchNum, match in enumerate(matches)]
+        sent = [s for s in re.split(reg, self._get_printable(p)) if s.strip()]
         for s in sent:
-            s = s.strip()
             if self._check_p(s):
                 if len(s) > self.max_len and reg != self.maybe_split:
                     for ss in self._split(s, self.maybe_split):
@@ -41,3 +37,8 @@ class SentenceSplitter:
 
     def split(self, p):
         return self._split(p, self.must_split)
+
+
+if __name__ == '__main__':
+    ss = SentenceSplitter()
+    print(list(ss.split('所述太阳能,   光伏发电,形  或。我是谁，谁是我，电位器，而我，的任务人分为2，所述太阳能,光伏发电,形或。我是谁，谁是我，电位器，而我，的任务人分为2')))
