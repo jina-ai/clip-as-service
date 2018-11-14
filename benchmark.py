56ab3a918d711dd8f4cdff2d2758742a9a2e2085
hanhxiao <hanhxiao@tencent.com>
2018-11-14 16:23:46 +0800

    set max_seq len to 40 in benchmark

--- benchmark.py
+++ benchmark.py
@@ -58,13 +58,13 @@ if __name__ == '__main__':
             'num_client': 1
         },
         {
-            'max_seq_len': 20,
+            'max_seq_len': 40,
             'max_batch_size': 128,
             'client_batch_size': [256, 1024, 2048, 4096],
             'num_client': 1,
         },
         {
-            'max_seq_len': 20,
+            'max_seq_len': 40,
             'max_batch_size': 128,
             'client_batch_size': 2048,
             'num_client': [1, 2, 3, 4],
