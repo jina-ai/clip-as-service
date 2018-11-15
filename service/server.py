22dd700cefcba43bb3b30ff7377da44930401cc5
hanhxiao <hanhxiao@tencent.com>
2018-11-15 20:55:14 +0800

    change to ipv4

--- service/server.py
+++ service/server.py
@@ -147,7 +147,7 @@ class BertSink(Process):
         self.receiver.bind(SINK_ADDR)
 
         self.frontend = self.context.socket(zmq.ROUTER)
-        self.frontend.connect('tcp://localhost:%d' % self.port)
+        self.frontend.connect('tcp://127.0.0.1:%d' % self.port)
         self.frontend.setsockopt(zmq.ROUTER_MANDATORY, 1)
 
         client_checksum = {}
