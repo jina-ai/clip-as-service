dae978a935dec95944c9fd8448318c4bc3acf26b
hanhxiao <hanhxiao@tencent.com>
2018-11-26 18:55:15 +0800

    add new example

--- example3.py
+++ example3.py
@@ -1,22 +1,27 @@
 # using BertClient in multi-cast way
 
 import sys
+import threading
 
 from service.client import BertClient
 
 
-# an endless data stream, generating data in an extremely fast speed
-def text_gen():
-    while True:
-        yield data
+def client_clone(id, idx):
+    bc = BertClient(ip='localhost', port=int(sys.argv[1]), port_out=int(sys.argv[2]),
+                    identity=id)
+    for j in bc.listen():
+        print('clone-client-%d: received %d x %d' % (idx, j.shape[0], j.shape[0]))
 
 
 if __name__ == '__main__':
     bc = BertClient(ip='localhost', port=int(sys.argv[1]), port_out=int(sys.argv[2]))
+    t1 = threading.Thread(target=client_clone, args=(bc.identity, 1))
+    t2 = threading.Thread(target=client_clone, args=(bc.identity, 2))
+    t1.start()
+    t2.start()
 
     with open('README.md') as fp:
         data = [v for v in fp if v.strip()]
 
-    # get encoded vectors
-    for j in bc.encode_async(text_gen()):
-        print('received %d x %d' % (j.shape[0], j.shape[0]))
+    for _ in range(3):
+        bc.encode(data)
