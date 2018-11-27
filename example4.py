a39cef8cd64ea84cf5c367e41c516e8eac848804
hanhxiao <hanhxiao@tencent.com>
2018-11-27 19:19:48 +0800

    restrict gpu visibility

--- example4.py
+++ example4.py
@@ -4,15 +4,23 @@ import os
 import GPUtil
 import tensorflow as tf
 
+from service.client import BertClient
+
 os.environ['CUDA_VISIBLE_DEVICES'] = str(GPUtil.getFirstAvailable())
 
 train_fp = ['/data/cips/data/lab/data/dataset/final_all_data/exercise_contest/data_train.json']
 batch_size = 256
 
+bc_clients = [BertClient() for _ in range(10)]
+
 
 def get_encodes(x):
     samples = [json.loads(l) for l in x]
     text = [s['fact'][-50:] for s in samples]
+    print(text)
+    # get a client from available clients
+    bc_client = bc_clients.pop()
+    print(bc_client.encode(text))
     return json.dumps(text, ensure_ascii=False).encode()
 
 
@@ -23,4 +31,4 @@ data_node = (tf.data.TextLineDataset(train_fp).batch(batch_size)
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     while True:
-        print(sess.run(data_node))
+        sess.run(data_node)
