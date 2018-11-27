e4c6fe0fb71a872b3534d90b4db2d67494f91675
hanhxiao <hanhxiao@tencent.com>
2018-11-27 19:43:57 +0800

    restrict gpu visibility

--- example4.py
+++ example4.py
@@ -10,23 +10,28 @@ os.environ['CUDA_VISIBLE_DEVICES'] = str(GPUtil.getFirstAvailable())
 
 train_fp = ['/data/cips/data/lab/data/dataset/final_all_data/exercise_contest/data_train.json']
 batch_size = 256
+num_parallel_calls = 4
+num_concurrent_clients = 10  # should be greater than `num_parallel_calls`
 
-bc_clients = [BertClient(show_server_config=False) for _ in range(10)]
+bc_clients = [BertClient(show_server_config=False) for _ in range(num_concurrent_clients)]
 
 
 def get_encodes(x):
     samples = [json.loads(l) for l in x]
     text = [s['fact'][-50:] for s in samples]
-    print(text)
     # get a client from available clients
     bc_client = bc_clients.pop()
-    print(bc_client.encode(text))
+    features = bc_client.encode(text)
+    # after use, put it back
     bc_clients.append(bc_client)
-    return json.dumps(text, ensure_ascii=False).encode()
+
+    labels = [0 for _ in text]
+    return features, labels
 
 
 data_node = (tf.data.TextLineDataset(train_fp).batch(batch_size)
-             .map(lambda x: tf.py_func(get_encodes, [x], tf.string, name='train_mktokens_fn'), num_parallel_calls=4)
+             .map(lambda x: tf.py_func(get_encodes, [x], [tf.float32, tf.int32], name='train_mktokens_fn'),
+                  num_parallel_calls=num_parallel_calls)
              .make_one_shot_iterator().get_next())
 
 with tf.Session() as sess:
