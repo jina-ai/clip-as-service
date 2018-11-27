994f206db379dfb07610ecbbf65ac12f349cd11f
hanhxiao <hanhxiao@tencent.com>
2018-11-27 21:03:34 +0800

    add new example for law article prediction

--- example5.py
+++ example5.py
@@ -46,7 +46,8 @@ def get_encodes(x):
 
 
 data_node = (tf.data.TextLineDataset(train_fp)
-             .apply(tf.contrib.data.shuffle_and_repeat(buffer_size=10000))
+             .shuffle(buffer_size=10000, reshuffle_each_iteration=True)
+             .repeat()
              .map(lambda x: tf.py_func(get_encodes, [x], [tf.float32, tf.int64], name='bert_client'),
                   num_parallel_calls=num_parallel_calls)
              .map(lambda x, y: {'feature': x, 'label': y})
