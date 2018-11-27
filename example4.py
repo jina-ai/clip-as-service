d0941205de2cf8f2cb4bddc0b3af1c87d5bc5f64
hanhxiao <hanhxiao@tencent.com>
2018-11-27 18:43:43 +0800

    update client print

---
+++ example4.py
@@ -0,0 +1,23 @@
+import json
+
+import tensorflow as tf
+
+train_fp = ['/data/cips/data/lab/data/dataset/final_all_data/exercise_contest/data_train.json']
+batch_size = 256
+
+
+def get_encodes(x):
+    samples = [json.loads(l) for l in x]
+    text = [s['fact'][-50:] for s in samples]
+    return json.dumps(text).encode()
+
+
+data_node = (tf.data.TextLineDataset(train_fp).batch(batch_size)
+             .map(lambda x: tf.py_func(get_encodes, [x], tf.string, name='train_mktokens_fn'),
+                  num_parallel_calls=4)
+             .make_one_shot_iterator().get_next())
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer)
+    while True:
+        print(sess.run(data_node))
