4baaa12b48031cce7d7fe2b883263f100cf8f4b5
hanhxiao <hanhxiao@tencent.com>
2018-11-28 12:14:15 +0800

    fix training

--- example5.py
+++ example5.py
@@ -9,7 +9,9 @@ from tensorflow.python.estimator.canned.dnn import DNNClassifier
 from gpu_env import MODEL_ID
 from service.client import BertClient
 
-os.environ['CUDA_VISIBLE_DEVICES'] = str(GPUtil.getFirstAvailable())
+print(GPUtil.getFirstAvailable())
+exit()
+os.environ['CUDA_VISIBLE_DEVICES'] = str()
 tf.logging.set_verbosity(tf.logging.INFO)
 
 train_fp = ['/data/cips/data/lab/data/dataset/final_all_data/exercise_contest/data_train.json']
