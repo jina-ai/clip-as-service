a55f210c82d9977d3b28b403cf2706dce9863f50
hanhxiao <hanhxiao@tencent.com>
2018-11-30 17:27:45 +0800

    add benchmark of pooling_layer

---
+++ example7.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+
+from service.client import BertClient
+
+with open('README.md') as fp:
+    data = [v for v in fp if v.strip()]
+    bc = BertClient()
+    list_vec = bc.encode(data)
+    list_label = [0 for _ in data]  # a dummy list of all-zero labels
+
+# write tfrecords
+
+with tf.python_io.TFRecordWriter('tmp.tfrecord') as writer:
+    def create_float_feature(values):
+        return tf.train.Feature(float_list=tf.train.FloatList(value=values))
+
+
+    def create_int_feature(values):
+        return tf.train.Feature(int64_list=tf.train.Int64List(value=list(values)))
+
+
+    for (vec, label) in zip(list_vec, list_label):
+        features = {'features': create_float_feature(vec), 'label_ids': create_int_feature([label])}
+        tf_example = tf.train.Example(features=tf.train.Features(feature=features))
+        writer.write(tf_example.SerializeToString())
+
+# read tfrecords and build dataset from it
+
+num_hidden_unit = 768
+
+
+def _decode_record(record):
+    """Decodes a record to a TensorFlow example."""
+    return tf.parse_single_example(record, {
+        "feature": tf.FixedLenFeature([num_hidden_unit], tf.float32),
+        "label_ids": tf.FixedLenFeature([], tf.int64),
+    })
+
+
+ds = (tf.data.TFRecordDataset('tmp.tfrecord').repeat().shuffle(buffer_size=100).apply(
+    tf.contrib.data.map_and_batch(lambda record: _decode_record(record), batch_size=64))
+      .make_one_shot_iterator().get_next())
+
+with tf.Session() as sess:
+    while True:
+        print(sess.run(ds))
