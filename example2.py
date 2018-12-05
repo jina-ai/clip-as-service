1b42512896d9fdd4ca77c7d31e6c5a6f064f15a6
hanhxiao <hanhxiao@tencent.com>
2018-12-05 15:06:09 +0800

    fix example2

--- example2.py
+++ example2.py
@@ -21,6 +21,13 @@ if __name__ == '__main__':
     with open('README.md') as fp:
         data = [v for v in fp if v.strip()]
 
+    # encoding without blocking:
+    for _ in range(10):
+        bc.encode(data, blocking=False)
+
+    # then fetch all
+    print(bc.fetch_all(concat=True))
+
     # get encoded vectors
     for j in bc.encode_async(text_gen(), max_num_batch=20):
         print('received %d : %s' % (j.id, j.content))
