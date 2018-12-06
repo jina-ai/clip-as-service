c638b0a7899a6d71ddf386f6e8a778da7132f601
hanhxiao <hanhxiao@tencent.com>
2018-12-06 13:02:54 +0800

    add pushpypi script

--- push2pypi.sh
+++ push2pypi.sh
@@ -4,7 +4,7 @@ CLIENT_CODE='client/bert_serving/client/__init__.py'
 SERVER_CODE='client/bert_serving/server/__init__.py'
 VER_TAG='__version__ = '
 
-VER=$(grep $VER_TAG $CLIENT_CODE | sed -n 's/^.*'\''\([^'\'']*\)'\''.*$/\1/p')
+VER=$(grep "$VER_TAG" $CLIENT_CODE | sed -n 's/^.*'\''\([^'\'']*\)'\''.*$/\1/p')
 echo 'current version: '$VER
 
 VER=$(echo $VER | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}')
