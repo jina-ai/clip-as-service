e97cdb5dceac779d803e9c02a49de2a128772329
hanhxiao <hanhxiao@tencent.com>
2018-12-11 16:36:42 +0800

    add logging

--- server/bert_serving/server/bert/extract_features.py
+++ server/bert_serving/server/bert/extract_features.py
@@ -166,10 +166,10 @@ def model_fn_builder(bert_config, init_checkpoint, use_one_hot_embeddings=False,
             #                  if '_XlaCompile' not in n.attr.keys()]))
 
             tmp_g = tf.get_default_graph().as_graph_def()
-            print('before : %d' % len(tmp_g), flush=True)
+            print('before : %d' % len(tmp_g.node), flush=True)
             print('__removed__', flush=True)
             tmp_g = tf.graph_util.remove_training_nodes(tmp_g)
-            print('after : %d' % len(tmp_g), flush=True)
+            print('after : %d' % len(tmp_g.node), flush=True)
 
             return EstimatorSpec(mode=mode, predictions={
                 'client_id': client_id,
