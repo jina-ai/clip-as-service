fe6bf130cfde3f329aa3225f28cc3897b27c645d
Nicolas REMOND <nicolas.remond@42maru.com>
2018-12-13 15:51:03 +0900

    Debug removed

--- server/bert_serving/server/__init__.py
+++ server/bert_serving/server/__init__.py
@@ -41,7 +41,7 @@ def _auto_bind(socket):
             tmp_dir = os.path.join(tmp_dir, str(uuid.uuid1())[:8])
         except KeyError:
             tmp_dir = '*'
-        print("DEBUG  = {}".format(tmp_dir))
+        
         socket.bind('ipc://{}'.format(tmp_dir))
     return socket.getsockopt(zmq.LAST_ENDPOINT).decode('ascii')
 
