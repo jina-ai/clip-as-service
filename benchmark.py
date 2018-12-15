96ffc7452eba10dbf5ced63a5b9586f2de9b4f69
hanhxiao <hanhxiao@tencent.com>
2018-12-15 22:13:54 +0800

    optimize import, refactor graph

--- benchmark.py
+++ benchmark.py
@@ -51,7 +51,7 @@ class BenchmarkClient(threading.Thread):
 
     def run(self):
         time_all = []
-        bc = BertClient(port=PORT, port_out=PORT_OUT, show_server_config=False)
+        bc = BertClient(port=PORT, port_out=PORT_OUT, show_server_config=False, check_version=False)
         for _ in range(self.num_repeat):
             start_t = time.perf_counter()
             bc.encode(self.batch)
