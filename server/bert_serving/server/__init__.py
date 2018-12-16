f38eca6f9c839226988e89b1dec2f54d2cff2d52
hanhxiao <hanhxiao@tencent.com>
2018-12-16 19:36:41 +0800

    increase version number

--- server/bert_serving/server/__init__.py
+++ server/bert_serving/server/__init__.py
@@ -22,7 +22,7 @@ from .bert.tokenization import FullTokenizer
 from .graph import optimize_graph
 from .helper import *
 
-__version__ = '1.5.5'
+__version__ = '1.5.6'
 
 _tf_ver_ = check_tf_version()
 
