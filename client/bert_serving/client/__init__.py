85d24894197e2c63cf86dafa9612ee17ca928c1e
Han Xiao <artex.xh@gmail.com>
2019-01-07 17:16:05 +0100

    Update __init__.py

--- client/bert_serving/client/__init__.py
+++ client/bert_serving/client/__init__.py
@@ -75,12 +75,10 @@ class BertClient:
 
         self.context = zmq.Context()
         self.sender = self.context.socket(zmq.PUSH)
-        self.sender.setsockopt(zmq.LINGER, 0)
         self.identity = identity or str(uuid.uuid4()).encode('ascii')
         self.sender.connect('tcp://%s:%d' % (ip, port))
 
         self.receiver = self.context.socket(zmq.SUB)
-        self.receiver.setsockopt(zmq.LINGER, 0)
         self.receiver.setsockopt(zmq.SUBSCRIBE, self.identity)
         self.receiver.connect('tcp://%s:%d' % (ip, port_out))
 
@@ -122,6 +120,8 @@ class BertClient:
             then this is not necessary.
 
         """
+        self.sender.setsockopt(zmq.LINGER, 0)
+        self.receiver.setsockopt(zmq.LINGER, 0)
         self.sender.close()
         self.receiver.close()
         self.context.term()
