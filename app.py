ea802de4de42e829c2c6507eadc074436536b824
hanhxiao <hanhxiao@tencent.com>
2019-01-15 14:54:19 +0800

    fix model

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def get_config():
     config = tf.ConfigProto(device_count={'GPU': 1})
     config.gpu_options.allow_growth = True
     config.gpu_options.per_process_gpu_memory_fraction = 0.5
-    config.log_device_placement = True
+    config.log_device_placement = False
     return config
 
 
