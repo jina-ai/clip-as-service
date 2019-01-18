40579decc7bfa6b5f38b00d9d727195c8e360078
hanhxiao <hanhxiao@tencent.com>
2019-01-18 14:56:21 +0800

    add fp16 to cli

--- server/bert_serving/server/graph.py
+++ server/bert_serving/server/graph.py
@@ -209,7 +209,7 @@ def convert_variables_to_constants(sess,
         elif input_node.op == "ReadVariableOp" and (
                 input_node.input[0] in found_variables):
             # placeholder nodes
-            print('%s | %s ' % (input_node.name, input_node.attr["dtype"]))
+            print('- %s | %s ' % (input_node.name, input_node.attr["dtype"]))
             output_node.op = "Identity"
             output_node.name = input_node.name
             output_node.input.extend([input_node.input[0]])
@@ -218,7 +218,7 @@ def convert_variables_to_constants(sess,
                 output_node.attr["_class"].CopyFrom(input_node.attr["_class"])
         else:
             # mostly op nodes
-            print('%s | %s ' % (input_node.name, input_node.attr["dtype"]))
+            print('* %s | %s ' % (input_node.name, input_node.attr["dtype"]))
             output_node.CopyFrom(input_node)
         output_graph_def.node.extend([output_node])
 
