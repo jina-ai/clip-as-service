b79a832b17e78060575fada6aa78dcd3083a9456
hanhxiao <hanhxiao@tencent.com>
2019-01-23 17:35:42 +0800

    increase version number

--- client/bert_serving/client/__init__.py
+++ client/bert_serving/client/__init__.py
@@ -16,7 +16,7 @@ from zmq.utils import jsonapi
 __all__ = ['__version__', 'BertClient']
 
 # in the future client version must match with server version
-__version__ = '1.7.7'
+__version__ = '1.7.8'
 
 if sys.version_info >= (3, 0):
     _py2 = False
