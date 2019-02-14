ec1fbf3d4b9c224f69ea13fecaed1cd7b318b55a
hanhxiao <hanhxiao@tencent.com>
2019-02-14 10:23:51 +0800

    use tf assert

--- server/bert_serving/server/bert/modeling.py
+++ server/bert_serving/server/bert/modeling.py
@@ -508,11 +508,14 @@ def embedding_postprocessor(input_tensor,
         # for position [0, 1, 2, ..., max_position_embeddings-1], and the current
         # sequence has positions [0, 1, 2, ... seq_length-1], so we can just
         # perform a slice.
-        if seq_length < max_position_embeddings:
-            position_embeddings = tf.slice(full_position_embeddings, [0, 0],
-                                           [seq_length, -1])
-        else:
-            position_embeddings = full_position_embeddings
+        position_embeddings = tf.cond(seq_length < max_position_embeddings,
+                                      lambda: tf.slice(full_position_embeddings, [0, 0], [seq_length, -1]),
+                                      lambda: full_position_embeddings)
+        # if seq_length < max_position_embeddings:
+        #     position_embeddings = tf.slice(full_position_embeddings, [0, 0],
+        #                                    [seq_length, -1])
+        # else:
+        #     position_embeddings = full_position_embeddings
 
         num_dims = len(output.shape.as_list())
 
