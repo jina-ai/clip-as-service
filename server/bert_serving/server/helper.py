7f3b2b43ff61dc6215364b32dbbbaaad66ed593c
hanhxiao <hanhxiao@tencent.com>
2019-03-14 11:28:44 +0800

    fix #266

--- server/bert_serving/server/helper.py
+++ server/bert_serving/server/helper.py
@@ -160,7 +160,7 @@ def check_tf_version():
     tf_ver = tf.__version__.split('.')
     if int(tf_ver[0]) <= 1 and int(tf_ver[1]) < 10:
         raise ModuleNotFoundError('Tensorflow >=1.10 (one-point-ten) is required!')
-    elif int(tf_ver[0]) > 2:
+    elif int(tf_ver[0]) > 1:
         warnings.warn('Tensorflow %s is not tested! It may or may not work. '
                       'Feel free to submit an issue at https://github.com/hanxiao/bert-as-service/issues/' % tf.__version__)
     return tf_ver
