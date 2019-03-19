4f5803ad265abb404b3690ab7fd15ef6638cf78f
hanhxiao <hanhxiao@tencent.com>
2019-03-19 14:26:37 +0800

    bumping version to v1.8.5

--- server/bert_serving/server/__init__.py
+++ server/bert_serving/server/__init__.py
@@ -24,7 +24,7 @@ from .http import BertHTTPProxy
 from .zmq_decor import multi_socket
 
 __all__ = ['__version__', 'BertServer']
-__version__ = '1.8.4'
+__version__ = '1.8.5'
 
 _tf_ver_ = check_tf_version()
 
