a39ff35b00cdc67d57010e5986ad37e0c6f50b47
hanhxiao <hanhxiao@tencent.com>
2019-03-20 14:49:36 +0800

    add timeout

--- server/bert_serving/server/__init__.py
+++ server/bert_serving/server/__init__.py
@@ -94,7 +94,7 @@ class BertServer(threading.Thread):
                     ctx.setsockopt(zmq.LINGER, args.timeout)
                     frontend.setsockopt(zmq.SNDTIMEO, args.timeout)
                     frontend.connect('tcp://localhost:%d' % args.port)
-                    frontend.send_multipart([b'', ServerCmd.terminate, b'', b''])
+                    frontend.send_multipart([b'', ServerCmd.terminate, b'', b''], zmq.NOBLOCK)
                     print('shutdown signal sent to %d' % args.port)
                 except zmq.error.Again:
                     raise TimeoutError(
