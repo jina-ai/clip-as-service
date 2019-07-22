a110b49fd1b4e066fa37aa1db63507d201012677
hanhxiao <hanhxiao@tencent.com>
2019-07-22 11:08:26 +0800

    bumping version to v1.9.4

--- client/bert_serving/client/__init__.py
+++ client/bert_serving/client/__init__.py
@@ -17,7 +17,7 @@ from zmq.utils import jsonapi
 __all__ = ['__version__', 'BertClient', 'ConcurrentBertClient']
 
 # in the future client version must match with server version
-__version__ = '1.9.3'
+__version__ = '1.9.4'
 
 if sys.version_info >= (3, 0):
     from ._py3_var import *
