8fa354a834b487632b1704bd28bcc42ac5eba013
Li Liangbin <liliangbin@soundai.com>
2020-06-10 10:32:10 +0800

    Refine log format.

--- server/bert_serving/server/helper.py
+++ server/bert_serving/server/helper.py
@@ -23,8 +23,7 @@ def set_logger(context, verbose=False):
     logger = logging.getLogger(context)
     logger.setLevel(logging.DEBUG if verbose else logging.INFO)
     formatter = logging.Formatter(
-        '%(levelname)-.1s:' + context + ':[%(filename).3s:%(funcName).3s:%(lineno)3d]:%(message)s', datefmt=
-        '%m-%d %H:%M:%S')
+        '[%(asctime)s] %(levelname)s ' + context + ' [%(filename)s:%(funcName)s:%(lineno)3d] %(message)s')
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.DEBUG if verbose else logging.INFO)
     console_handler.setFormatter(formatter)
