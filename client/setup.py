0ec1fac2a4f0c02a0cc704da9b2361e1d22108fa
Han Xiao <han.xiao@jina.ai>
2022-03-24 11:56:49 +0100

    chore: fix setup deps

--- client/setup.py
+++ client/setup.py
@@ -41,7 +41,7 @@ setup(
     long_description_content_type='text/markdown',
     zip_safe=False,
     setup_requires=['setuptools>=18.0', 'wheel'],
-    install_requires=['jina'],
+    install_requires=['jina', 'docarray[common]>=0.9.18'],
     extras_require={
         'test': [
             'pytest',
