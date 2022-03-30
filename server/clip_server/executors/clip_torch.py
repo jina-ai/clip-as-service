8812343211701fd93b75dab9f5b86bd5bc9e6819
Han Xiao <han.xiao@jina.ai>
2022-03-31 01:19:04 +0200

    perf(server): use map_batch to overlap cpu gpu (#669)

--- server/clip_server/executors/clip_torch.py
+++ server/clip_server/executors/clip_torch.py
@@ -1,15 +1,13 @@
 import io
-import os
-from typing import TYPE_CHECKING, Optional, List
+from typing import TYPE_CHECKING, Optional, List, Tuple
 
 import torch
 from PIL import Image
+from clip_server.model import clip
 from jina import Executor, requests
 
-from clip_server.model import clip
-
 if TYPE_CHECKING:
-    from docarray import DocumentArray, Document
+    from docarray import DocumentArray
 
 
 class CLIPEncoder(Executor):
@@ -18,6 +16,8 @@ class CLIPEncoder(Executor):
         name: str = 'ViT-B/32',
         device: Optional[str] = None,
         jit: bool = False,
+        num_worker_preprocess: int = 4,
+        minibatch_size: int = 64,
         **kwargs
     ):
         super().__init__(**kwargs)
@@ -25,17 +25,21 @@ class CLIPEncoder(Executor):
             self._device = 'cuda' if torch.cuda.is_available() else 'cpu'
         else:
             self._device = device
+        self._num_worker_preprocess = num_worker_preprocess
+        self._minibatch_size = minibatch_size
         self._model, self._preprocess = clip.load(name, device=self._device, jit=jit)
 
-    def _preproc_image(self, d: 'Document'):
-        d.tensor = self._preprocess(Image.open(io.BytesIO(d.blob))).to(self._device)
-        return d
+    def _preproc_image(self, da: 'DocumentArray') -> 'DocumentArray':
+        for d in da:
+            d.tensor = self._preprocess(Image.open(io.BytesIO(d.blob)))
+        da.tensors = da.tensors.to(self._device)
+        return da
 
-    def _preproc_text(self, da: 'DocumentArray') -> List[str]:
+    def _preproc_text(self, da: 'DocumentArray') -> Tuple['DocumentArray', List[str]]:
         texts = da.texts
-        da.tensors = clip.tokenize(da.texts).to(self._device)
+        da.tensors = clip.tokenize(texts).to(self._device)
         da[:, 'mime_type'] = 'text'
-        return texts
+        return da, texts
 
     @requests
     async def encode(self, docs: 'DocumentArray', **kwargs):
@@ -45,18 +49,26 @@ class CLIPEncoder(Executor):
         with torch.inference_mode():
             # for image
             if _img_da:
-                _img_da.apply(self._preproc_image)
-                _img_da.embeddings = (
-                    self._model.encode_image(_img_da.tensors).cpu().numpy()
-                )
+                for minibatch in _img_da.map_batch(
+                    self._preproc_image,
+                    batch_size=self._minibatch_size,
+                    num_worker=self._num_worker_preprocess,
+                ):
+                    minibatch.embeddings = (
+                        self._model.encode_image(minibatch.tensors).cpu().numpy()
+                    )
 
             # for text
             if _txt_da:
-                texts = self._preproc_text(_txt_da)
-                _txt_da.embeddings = (
-                    self._model.encode_text(_txt_da.tensors).cpu().numpy()
-                )
-                _txt_da.texts = texts
+                for minibatch, _texts in _txt_da.map_batch(
+                    self._preproc_text,
+                    batch_size=self._minibatch_size,
+                    num_worker=self._num_worker_preprocess,
+                ):
+                    minibatch.embeddings = (
+                        self._model.encode_text(minibatch.tensors).cpu().numpy()
+                    )
+                    minibatch.texts = _texts
 
         # drop tensors
         docs.tensors = None
