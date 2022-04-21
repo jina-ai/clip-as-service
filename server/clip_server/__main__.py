2558d7388a6ebdbfa99e489d9d005d9e1c22c33a
felix-wang <35718120+numb3r3@users.noreply.github.com>
2022-04-21 23:31:09 +0800

    fix: pass extra_search path (#687)

--- server/clip_server/__main__.py
+++ server/clip_server/__main__.py
@@ -1,8 +1,12 @@
 import sys
-
+import os
+import inspect
 from jina import Flow
 
 if __name__ == '__main__':
-    f = Flow.load_config('torch-flow.yml' if len(sys.argv) == 1 else sys.argv[1])
+    f = Flow.load_config(
+        'torch-flow.yml' if len(sys.argv) == 1 else sys.argv[1],
+        extra_search_paths=[os.path.dirname(inspect.getfile(inspect.currentframe()))],
+    )
     with f:
         f.block()
