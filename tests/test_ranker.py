a1d011cf0bb8e79fb62b1917e680e742bf695e4b
Han Xiao <han.xiao@jina.ai>
2022-04-25 16:07:57 +0200

    feat(server): add rank endpoint

--- tests/test_ranker.py
+++ tests/test_ranker.py
@@ -1,8 +1,10 @@
 import os
 
 import pytest
+from clip_client import Client
 from clip_server.executors.clip_torch import CLIPEncoder
 from docarray import DocumentArray, Document
+from jina import Flow
 
 
 @pytest.mark.asyncio
@@ -40,3 +42,35 @@ async def test_torch_executor_rank_text2imgs():
     for d in db:
         for c in d.matches:
             assert c.scores['clip-rank'].value is not None
+
+
+@pytest.mark.parametrize(
+    'd',
+    [
+        Document(
+            uri='https://docarray.jina.ai/_static/favicon.png',
+            matches=[Document(text='hello, world'), Document(text='goodbye, world')],
+        ),
+        Document(
+            text='hello, world',
+            matches=[
+                Document(uri='https://docarray.jina.ai/_static/favicon.png'),
+                Document(
+                    uri=f'{os.path.dirname(os.path.abspath(__file__))}/img/00000.jpg'
+                ),
+            ],
+        ),
+    ],
+)
+def test_docarray_inputs(d, port_generator):
+    from clip_server.executors.clip_torch import CLIPEncoder
+
+    f = Flow(port=port_generator()).add(uses=CLIPEncoder)
+    with f:
+        c = Client(server=f'grpc://0.0.0.0:{f.port}')
+        r = c.rerank([d])
+    assert isinstance(r, DocumentArray)
+    rv = r['@m', 'scores__clip-rank__value']
+    for v in rv:
+        assert v is not None
+        assert v > 0
