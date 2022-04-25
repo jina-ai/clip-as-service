a4a23ff98aa0849d7c7d274f0120b04fce837263
numb3r3 <wangfelix87@gmail.com>
2022-04-25 11:11:32 +0800

    fix: setup

--- server/setup.py
+++ server/setup.py
@@ -56,7 +56,7 @@ setup(
             'onnx',
         ]
         + (['onnxruntime-gpu>=1.8.0'] if sys.platform != 'darwin' else []),
-        'tensorrt': ['onnx', 'onnxruntime', 'onnxruntime-gpu', 'nvidia-tensorrt'],
+        'tensorrt': ['nvidia-tensorrt'],
     },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
