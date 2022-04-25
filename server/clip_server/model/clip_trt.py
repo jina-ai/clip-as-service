a92090754ec67d13aaf8b509529f3c82a5463b5d
numb3r3 <wangfelix87@gmail.com>
2022-04-25 11:16:25 +0800

    fix: s3 bucket for tensorrt

--- server/clip_server/model/clip_trt.py
+++ server/clip_server/model/clip_trt.py
@@ -16,7 +16,7 @@ except ImportError:
 
 from .clip import _download, available_models
 
-_S3_BUCKET = 'https://clip-as-service.s3.us-east-2.amazonaws.com/models/onnx/'
+_S3_BUCKET = 'https://clip-as-service.s3.us-east-2.amazonaws.com/models/tensorrt/'
 _MODELS = {
     'RN50': ('RN50/textual.trt', 'RN50/visual.trt'),
     'RN101': ('RN101/textual.trt', 'RN101/visual.trt'),
