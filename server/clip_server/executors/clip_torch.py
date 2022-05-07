fix: use softmax as para name

server/clip_server/executors/clip_torch.py
@@ -62,7 +62,7 @@ class CLIPEncoder(Executor):
         import torch
 
         _source = parameters.get('source', 'matches')
-        _score = parameters.get('score', 'probability')
+        _score = parameters.get('score', 'softmax')
 
         for d in docs:
             _img_da = DocumentArray()
@@ -109,7 +109,7 @@ class CLIPEncoder(Executor):
                 elif len(_txt_da) == 1:
                     scores = scores_per_image
 
-                if _score == 'probability':
+                if _score == 'softmax':
                     scores = self._logit_scale * scores
                     scores = scores.softmax(dim=-1)
 