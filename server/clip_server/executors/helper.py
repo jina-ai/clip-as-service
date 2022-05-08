fix: numpy softmax

server/clip_server/executors/helper.py
@@ -6,13 +6,12 @@ if TYPE_CHECKING:
     from docarray import Document, DocumentArray
 
 
-def numpy_softmax(z: 'np.ndarray') -> 'np.ndarray':
-    s = np.max(z, axis=1)
-    s = s[:, np.newaxis]
-    e_x = np.exp(z - s)
-    div = np.sum(e_x, axis=1)
-    div = div[:, np.newaxis]
-    return e_x / div
+def numpy_softmax(x: 'np.ndarray', axis: int = -1) -> 'np.ndarray':
+    max = np.max(x, axis=axis, keepdims=True)
+    e_x = np.exp(x - max)
+    div = np.sum(e_x, axis=axis, keepdims=True)
+    f_x = e_x / div
+    return f_x
 
 
 def preproc_image(