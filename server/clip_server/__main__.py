cf7f70cffef0ba043b7ee498fefb517dbbe701f7
Han Xiao <han.xiao@jina.ai>
2022-06-20 15:18:19 +0200

    feat: read config from stdin to allow pipe

--- server/clip_server/__main__.py
+++ server/clip_server/__main__.py
@@ -10,8 +10,16 @@ if __name__ == '__main__':
 
     from jina import Flow
 
+    if len(sys.argv) > 1:
+        if sys.argv[1] == '-i':
+            _input = sys.stdin.read()
+        else:
+            _input = sys.argv[1]
+    else:
+        _input = 'torch-flow.yml'
+
     f = Flow.load_config(
-        'torch-flow.yml' if len(sys.argv) == 1 else sys.argv[1],
+        _input,
         extra_search_paths=[os.path.dirname(inspect.getfile(inspect.currentframe()))],
     )
     with f:
