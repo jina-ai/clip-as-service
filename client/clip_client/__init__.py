79e85eed7c89f31c16399bfcc1bb098f0ae5c920
Han Xiao <han.xiao@jina.ai>
2022-06-21 17:43:37 +0200

    fix: miscalling clip_server in clip_client

--- client/clip_client/__init__.py
+++ client/clip_client/__init__.py
@@ -5,6 +5,6 @@ import os
 from clip_client.client import Client
 
 if 'NO_VERSION_CHECK' not in os.environ:
-    from clip_server.helper import is_latest_version
+    from clip_client.helper import is_latest_version
 
     is_latest_version(github_repo='clip-as-service')
