fix: add attention mask for onnx

server/clip_server/executors/clip_torch.py
@@ -126,7 +126,7 @@ class CLIPEncoder(Executor):
                         documentation='texts encode time in seconds',
                     ):
                         minibatch.embeddings = (
-                            self._model.encode_text(batch_data)
+                            self._model.encode_text(batch_data['input_ids'])
                             .cpu()
                             .numpy()
                             .astype(np.float32)

server/clip_server/executors/helper.py
@@ -52,16 +52,20 @@ def preproc_text(
     da: 'DocumentArray', device: str = 'cpu', return_np: bool = False
 ) -> Tuple['DocumentArray', List[Any]]:
 
-    tokenized = clip.tokenize(da.texts)
-    tokenized['input_ids'] = tokenized['input_ids'].detach()
+    inputs = clip.tokenize(da.texts)
+    inputs['input_ids'] = inputs['input_ids'].detach()
 
     if return_np:
-        tokenized['input_ids'] = tokenized['input_ids'].cpu().numpy().astype(np.int32)
+        inputs['input_ids'] = inputs['input_ids'].cpu().numpy().astype(np.int32)
+        inputs['attention_mask'] = (
+            inputs['attention_mask'].cpu().numpy().astype(np.int32)
+        )
     else:
-        tokenized['input_ids'] = tokenized['input_ids'].to(device)
+        inputs['input_ids'] = inputs['input_ids'].to(device)
+        inputs['attention_mask'] = inputs['attention_mask'].to(device)
 
     da[:, 'mime_type'] = 'text'
-    return da, tokenized
+    return da, inputs
 
 
 def split_img_txt_da(doc: 'Document', img_da: 'DocumentArray', txt_da: 'DocumentArray'):

server/clip_server/model/clip.py
@@ -24,7 +24,7 @@ except ImportError:
 __all__ = ['available_models', 'load', 'tokenize']
 _tokenizer = _Tokenizer()
 
-_S3_BUCKET = 'https://clip-as-service.s3.us-east-2.amazonaws.com/modelsV2/torch/'
+_S3_BUCKET = 'https://clip-as-service.s3.us-east-2.amazonaws.com/models/torch/'
 _MODELS = {
     'RN50': 'RN50.pt',
     'RN101': 'RN101.pt',
@@ -193,7 +193,7 @@ def load(
         Whether to load the optimized JIT model or more hackable non-JIT model (default).
 
     download_root: str
-        path to download the model files; by default, it uses '~/.cache/clip/v2'
+        path to download the model files; by default, it uses '~/.cache/clip/'
 
     Returns
     -------
@@ -206,7 +206,7 @@ def load(
     if name in _MODELS:
         model_path = _download(
             _S3_BUCKET + _MODELS[name],
-            download_root or os.path.expanduser('~/.cache/clip/v2'),
+            download_root or os.path.expanduser('~/.cache/clip'),
             with_resume=True,
         )
     elif os.path.isfile(name):
@@ -335,8 +335,14 @@ def tokenize(
     sot_token = _tokenizer.encoder['<|startoftext|>']
     eot_token = _tokenizer.encoder['<|endoftext|>']
     all_tokens = [[sot_token] + _tokenizer.encode(text) + [eot_token] for text in texts]
-    result = torch.zeros(len(all_tokens), context_length, dtype=torch.long)
-    attention_masks = torch.zeros(len(all_tokens), context_length, dtype=torch.long)
+    # input_ids = torch.zeros(
+    #     len(all_tokens), min(len(max(all_tokens, key=len)), context_length), dtype=torch.long
+    # )
+    # attention_masks = torch.zeros(
+    #     len(all_tokens), min(len(max(all_tokens, key=len)), context_length), dtype=torch.long
+    # )
+    input_ids = torch.zeros(len(all_tokens), context_length, dtype=torch.long)
+    attention_mask = torch.zeros(len(all_tokens), context_length, dtype=torch.long)
 
     for i, tokens in enumerate(all_tokens):
         if len(tokens) > context_length:
@@ -347,6 +353,7 @@ def tokenize(
                 raise RuntimeError(
                     f'Input {texts[i]} is too long for context length {context_length}'
                 )
-        result[i, : len(tokens)] = torch.tensor(tokens)
-        attention_masks[i, : len(tokens)] = 1
-    return {'input_ids': result, 'attention_mask': attention_masks}
+        input_ids[i, : len(tokens)] = torch.tensor(tokens)
+        attention_mask[i, : len(tokens)] = 1
+
+    return {'input_ids': input_ids, 'attention_mask': attention_mask}