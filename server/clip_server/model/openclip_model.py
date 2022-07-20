ff1116125989ab5b6e56c1d6623de884acf80479
felix-wang <35718120+numb3r3@users.noreply.github.com>
2022-07-20 16:42:55 +0800

    fix: refactor preprocessor (#776)
    * fix: refactor preprocessor
    
    * fix: error
    
    * fix: missing functions

--- server/clip_server/model/openclip_model.py
+++ server/clip_server/model/openclip_model.py
@@ -1,21 +1,34 @@
-import open_clip
-import torch
+# Originally from https://github.com/mlfoundations/open_clip.
+#
+# Copyright (c) 2012-2021 Gabriel Ilharco, Mitchell Wortsman,
+# Nicholas Carlini, Rohan Taori, Achal Dave, Vaishaal Shankar,
+# John Miller, Hongseok Namkoong, Hannaneh Hajishirzi, Ali Farhadi,
+# Ludwig Schmidt
+
+from typing import TYPE_CHECKING
 
 from clip_server.model.clip_model import CLIPModel
+import open_clip
+
+if TYPE_CHECKING:
+    import torch
 
 
 class OpenCLIPModel(CLIPModel):
-    def __init__(self, name: str, device: str, jit: bool):
-        super().__init__(name, device, jit)
-        name, pretrained = name.split('::')
+    def __init__(self, name: str, device: str = 'cpu', jit: bool = False, **kwargs):
+        super().__init__(name, **kwargs)
+        model_name, pretrained = name.split('::')
         self._model = open_clip.create_model(
-            name, pretrained=pretrained, device=device, jit=jit
+            model_name, pretrained=pretrained, device=device, jit=jit
         )
+        self._model_name = model_name
 
-    def encode_text(
-        self, input_ids: torch.Tensor, attention_mask: torch.Tensor, **kwargs
-    ):
-        return self._model.encode_text(input_ids, **kwargs)
+    @property
+    def visual_model_name(self):
+        return self._model_name
 
-    def encode_image(self, pixel_values: torch.Tensor, **kwargs):
-        return self._model.encode_image(pixel_values, **kwargs)
+    def encode_text(self, input_ids: 'torch.Tensor', **kwargs):
+        return self._model.encode_text(input_ids)
+
+    def encode_image(self, pixel_values: 'torch.Tensor', **kwargs):
+        return self._model.encode_image(pixel_values)
