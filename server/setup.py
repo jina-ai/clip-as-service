e21bf7057abef5784a0711968ce2bf4bf7aa8ff4
ZiniuYu <ziniuyu@gmail.com>
2022-07-29 15:19:13 +0800

    fix: tmp set grpcio version

--- server/setup.py
+++ server/setup.py
@@ -50,6 +50,9 @@ setup(
         'jina>=3.6.0',
         'prometheus-client',
         'open_clip_torch>=1.3.0',
+        'grpcio>=1.46.0,<1.48.0',  # tmp requires
+        'grpcio-reflection>=1.46.0,<1.48.0',  # tmp requires
+        'grpcio-health-checking>=1.46.0,<1.48.0',  # tmp requires
     ],
     extras_require={
         'onnx': [
