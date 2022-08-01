4a4dfcbdcf1baaee01a5a5f2f665310cfc35020b
numb3r3 <wangfelix87@gmail.com>
2022-08-01 17:00:22 +0800

    fix: clone open clip model

--- server/clip_server/model/clip.py
+++ server/clip_server/model/clip.py
@@ -1,11 +1,6 @@
 # Originally from https://github.com/openai/CLIP. MIT License, Copyright (c) 2021 OpenAI
 
 import io
-import os
-import hashlib
-import shutil
-import urllib
-from typing import List
 
 from PIL import Image
 from torchvision.transforms import Compose, Resize, CenterCrop, ToTensor, Normalize
@@ -16,135 +11,6 @@ try:
     BICUBIC = InterpolationMode.BICUBIC
 except ImportError:
     BICUBIC = Image.BICUBIC
-
-
-_S3_BUCKET = 'https://clip-as-service.s3.us-east-2.amazonaws.com/models/torch/'
-_MODELS = {
-    'RN50': ('RN50.pt', '9140964eaaf9f68c95aa8df6ca13777c'),
-    'RN101': ('RN101.pt', 'fa9d5f64ebf152bc56a18db245071014'),
-    'RN50x4': ('RN50x4.pt', '03830990bc768e82f7fb684cde7e5654'),
-    'RN50x16': ('RN50x16.pt', '83d63878a818c65d0fb417e5fab1e8fe'),
-    'RN50x64': ('RN50x64.pt', 'a6631a0de003c4075d286140fc6dd637'),
-    'ViT-B/32': ('ViT-B-32.pt', '3ba34e387b24dfe590eeb1ae6a8a122b'),
-    'ViT-B/16': ('ViT-B-16.pt', '44c3d804ecac03d9545ac1a3adbca3a6'),
-    'ViT-L/14': ('ViT-L-14.pt', '096db1af569b284eb76b3881534822d9'),
-    'ViT-L/14@336px': ('ViT-L-14-336px.pt', 'b311058cae50cb10fbfa2a44231c9473'),
-}
-
-MODEL_SIZE = {
-    'RN50': 224,
-    'RN101': 224,
-    'RN50x4': 288,
-    'RN50x16': 384,
-    'RN50x64': 448,
-    'ViT-B/32': 224,
-    'ViT-B/16': 224,
-    'ViT-L/14': 224,
-    'ViT-L/14@336px': 336,
-}
-
-
-def md5file(filename: str):
-    hash_md5 = hashlib.md5()
-    with open(filename, 'rb') as f:
-        for chunk in iter(lambda: f.read(4096), b""):
-            hash_md5.update(chunk)
-
-    return hash_md5.hexdigest()
-
-
-def _download(
-    url: str,
-    target_folder: str,
-    md5sum: str = None,
-    with_resume: bool = True,
-    max_attempts: int = 3,
-) -> str:
-    os.makedirs(target_folder, exist_ok=True)
-    filename = os.path.basename(url)
-
-    download_target = os.path.join(target_folder, filename)
-
-    if os.path.exists(download_target):
-        if not os.path.isfile(download_target):
-            raise FileExistsError(f'{download_target} exists and is not a regular file')
-
-        actual_md5sum = md5file(download_target)
-        if (not md5sum) or actual_md5sum == md5sum:
-            return download_target
-
-    from rich.progress import (
-        DownloadColumn,
-        Progress,
-        TextColumn,
-        TimeRemainingColumn,
-        TransferSpeedColumn,
-    )
-
-    progress = Progress(
-        TextColumn("[bold blue]{task.fields[filename]}", justify="right"),
-        "[progress.percentage]{task.percentage:>3.1f}%",
-        "•",
-        DownloadColumn(),
-        "•",
-        TransferSpeedColumn(),
-        "•",
-        TimeRemainingColumn(),
-    )
-
-    with progress:
-        task = progress.add_task('download', filename=url, start=False)
-
-        for _ in range(max_attempts):
-            tmp_file_path = download_target + '.part'
-            resume_byte_pos = (
-                os.path.getsize(tmp_file_path) if os.path.exists(tmp_file_path) else 0
-            )
-
-            try:
-                # resolve the 403 error by passing a valid user-agent
-                req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-                total_bytes = int(
-                    urllib.request.urlopen(req).info().get('Content-Length', -1)
-                )
-                mode = 'ab' if (with_resume and resume_byte_pos) else 'wb'
-
-                with open(tmp_file_path, mode) as output:
-                    progress.update(task, total=total_bytes)
-                    progress.start_task(task)
-
-                    if resume_byte_pos and with_resume:
-                        progress.update(task, advance=resume_byte_pos)
-                        req.headers['Range'] = f'bytes={resume_byte_pos}-'
-
-                    with urllib.request.urlopen(req) as source:
-                        while True:
-                            buffer = source.read(8192)
-                            if not buffer:
-                                break
-
-                            output.write(buffer)
-                            progress.update(task, advance=len(buffer))
-
-                actual_md5 = md5file(tmp_file_path)
-                if (md5sum and actual_md5 == md5sum) or (not md5sum):
-                    shutil.move(tmp_file_path, download_target)
-                    return download_target
-                else:
-                    os.remove(tmp_file_path)
-                    raise RuntimeError(
-                        f'MD5 mismatch: expected {md5sum}, got {actual_md5}'
-                    )
-
-            except Exception as ex:
-                progress.console.print(
-                    f'Failed to download {url} with {ex!r} at the {_}th attempt'
-                )
-                progress.reset(task)
-
-        raise RuntimeError(
-            f'Failed to download {url} within retry limit {max_attempts}'
-        )
 
 
 def _convert_image_to_rgb(image):
@@ -183,8 +49,3 @@ def _transform_ndarray(n_px):
             ),
         ]
     )
-
-
-def available_models() -> List[str]:
-    '''Returns the names of available CLIP models'''
-    return list(_MODELS.keys())
--- server/clip_server/model/openclip_model.py
+++ server/clip_server/model/openclip_model.py
@@ -7,30 +7,85 @@
 
 from typing import TYPE_CHECKING
 
+from copy import deepcopy
+
 from clip_server.model.clip_model import CLIPModel
 from clip_server.model.pretrained_models import get_model_url_md5, download_model
-import open_clip
+from clip_server.model.model import CLIP, convert_weights_to_fp16
+
 from open_clip.openai import load_openai_model
+from open_clip.factory import _MODEL_CONFIGS, load_checkpoint
 
 if TYPE_CHECKING:
     import torch
+
+
+def _load_model(
+    model_name: str,
+    model_path: str,
+    device: torch.device = torch.device('cpu'),
+    jit: bool = False,
+    force_quick_gelu: bool = False,
+    pretrained_image: bool = False,
+):
+    model_name = model_name.replace(
+        '/', '-'
+    )  # for callers using old naming with / in ViT names
+
+    if model_name in _MODEL_CONFIGS:
+        model_cfg = deepcopy(_MODEL_CONFIGS[model_name])
+    else:
+        raise RuntimeError(f'Model config for {model_name} not found.')
+
+    if force_quick_gelu:
+        # override for use of QuickGELU on non-OpenAI transformer models
+        model_cfg["quick_gelu"] = True
+
+    if pretrained_image:
+        if 'timm_model_name' in model_cfg.get('vision_cfg', {}):
+            # pretrained weight loading for timm models set via vision_cfg
+            model_cfg['vision_cfg']['timm_model_pretrained'] = True
+        else:
+            assert (
+                False
+            ), 'pretrained image towers currently only supported for timm models'
+
+    model = CLIP(**model_cfg)
+    model.eval()
+
+    load_checkpoint(model, model_path)
+    if str(device).startswith('cuda'):
+        convert_weights_to_fp16(model)
+
+    model.to(device=device)
+
+    if jit:
+        model = torch.jit.script(model)
+
+    return model
 
 
 class OpenCLIPModel(CLIPModel):
     def __init__(self, name: str, device: str = 'cpu', jit: bool = False, **kwargs):
         super().__init__(name, **kwargs)
 
-        model_url, md5sum = get_model_url_md5(name)
-        if model_url:
-            model_path = download_model(model_url, md5sum=md5sum)
-            self._model = load_openai_model(model_path, device=device, jit=jit)
-            self._model_name = name
-        else:
+        if '::' in name:
             model_name, pretrained = name.split('::')
-            self._model = open_clip.create_model(
-                model_name, pretrained=pretrained, device=device, jit=jit
+        else:
+            model_name = name
+            pretrained = 'openai'
+
+        self._model_name = model_name
+
+        model_url, md5sum = get_model_url_md5(name)
+        model_path = download_model(model_url, md5sum=md5sum)
+
+        if pretrained == 'openai':
+            self._model = load_openai_model(model_path, device=device, jit=jit)
+        else:
+            self._model = _load_model(
+                self._model_name, model_path=model_path, device=device, jit=jit
             )
-            self._model_name = model_name
 
     @staticmethod
     def get_model_name(name: str):
