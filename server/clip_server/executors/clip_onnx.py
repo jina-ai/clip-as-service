c2b05a23efc55bec3b260ef91a6495b8b276f7f1
ZiniuYu <ziniuyu@gmail.com>
2022-08-03 10:49:23 +0800

    fix: change onnx default model name to ViT-B-32::openai

--- server/clip_server/executors/clip_onnx.py
+++ server/clip_server/executors/clip_onnx.py
@@ -19,7 +19,7 @@ from jina import Executor, requests, DocumentArray
 class CLIPEncoder(Executor):
     def __init__(
         self,
-        name: str = 'ViT-B/32',
+        name: str = 'ViT-B-32::openai',
         device: Optional[str] = None,
         num_worker_preprocess: int = 4,
         minibatch_size: int = 32,
