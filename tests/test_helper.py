b65445d18d7af7fb1848e264815bc7acabbf0e4c
ZiniuYu <ziniuyu@gmail.com>
2022-08-04 14:44:11 +0800

    test: add split da test

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,6 +1,8 @@
 import pytest
 import numpy as np
 from clip_server.executors.helper import numpy_softmax
+from clip_server.executors.helper import split_img_txt_da
+from docarray import Document, DocumentArray
 
 
 @pytest.mark.parametrize('shape', [(5, 10), (5, 10, 10)])
@@ -17,3 +19,42 @@ def test_numpy_softmax(shape, axis):
     np_softmax = numpy_softmax(logits, axis=axis)
     torch_softmax = torch.from_numpy(logits).softmax(dim=axis).numpy()
     np.testing.assert_array_almost_equal(np_softmax, torch_softmax)
+
+
+@pytest.mark.parametrize(
+    'inputs',
+    [
+        (
+            DocumentArray(
+                [
+                    Document(text='hello, world'),
+                    Document(text='goodbye, world'),
+                    Document(
+                        text='hello, world',
+                        uri='https://docarray.jina.ai/_static/favicon.png',
+                    ),
+                    Document(
+                        uri='https://docarray.jina.ai/_static/favicon.png',
+                    ),
+                ]
+            ),
+            (3, 1),
+        ),
+        (
+            DocumentArray(
+                [
+                    Document(text='hello, world'),
+                    Document(uri='https://docarray.jina.ai/_static/favicon.png'),
+                ]
+            ),
+            (1, 1),
+        ),
+    ],
+)
+def test_split_img_txt_da(inputs):
+    txt_da = DocumentArray()
+    img_da = DocumentArray()
+    for doc in inputs[0]:
+        split_img_txt_da(doc, txt_da, img_da)
+    assert len(txt_da) == inputs[1][0]
+    assert len(img_da) == inputs[1][1]
