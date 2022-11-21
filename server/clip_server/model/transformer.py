ba3aa44faae504ebbb7b77e7f59f878e0beeabb1
ZiniuYu <ziniuyu@gmail.com>
2022-11-21 15:10:26 +0800

    fix: refactor

--- server/clip_server/model/transformer.py
+++ server/clip_server/model/transformer.py
@@ -11,11 +11,11 @@ from typing import Callable, Optional
 
 import torch
 from torch import nn
-from torch.nn import functional as F
-from torch.utils.checkpoint import checkpoint
-
-from open_clip.utils import to_2tuple
 from open_clip.transformer import LayerNorm
+from open_clip.transformer import ResidualAttentionBlock as _ResidualAttentionBlock
+from open_clip.transformer import Transformer as _Transformer
+from open_clip.transformer import VisionTransformer as _VisionTransformer
+from open_clip.transformer import TextTransformer as _TextTransformer
 
 # Use flash attention
 try:
@@ -26,71 +26,50 @@ except:
     FLASH_ATTENTION_AVAILABLE = False
 
 
-class ResidualAttentionBlock(nn.Module):
+class ResidualAttentionBlock(_ResidualAttentionBlock):
     def __init__(
         self,
         d_model: int,
         n_head: int,
         mlp_ratio: float = 4.0,
+        ls_init_value: float = None,
         act_layer: Callable = nn.GELU,
         norm_layer: Callable = LayerNorm,
-        scale_attn: bool = False,
-        scale_fc: bool = False,
-        dtype='fp32',
+        # scale_attn: bool = False,
+        # scale_fc: bool = False,
+        dtype=torch.float32,
     ):
-        super().__init__()
+        super().__init__(
+            d_model, n_head, mlp_ratio, ls_init_value, act_layer, norm_layer
+        )
         head_dim = d_model // n_head
-        self.flash_attention = head_dim % 8 == 0 and head_dim <= 128
+        flash_attention = head_dim % 8 == 0 and head_dim <= 128
 
-        self.ln_1 = norm_layer(d_model)
         self.attn = (
             MultiheadAttention(d_model, n_head)
             if FLASH_ATTENTION_AVAILABLE
             and torch.cuda.is_available()
-            and dtype in ('fp16', 'bf16')
-            and self.flash_attention
+            and dtype in (torch.float16, torch.bfloat16)
+            and flash_attention
             else nn.MultiheadAttention(d_model, n_head)
         )
-        self.ln_attn = LayerNorm(d_model) if scale_attn else nn.Identity()
-
-        self.ln_2 = norm_layer(d_model)
-        mlp_width = int(d_model * mlp_ratio)
-        self.mlp = nn.Sequential(
-            OrderedDict(
-                [
-                    ("c_fc", nn.Linear(d_model, mlp_width)),
-                    ('ln', norm_layer(mlp_width) if scale_fc else nn.Identity()),
-                    ("gelu", act_layer()),
-                    ("c_proj", nn.Linear(mlp_width, d_model)),
-                ]
-            )
-        )
-
-    def attention(self, x: torch.Tensor, attn_mask: Optional[torch.Tensor] = None):
-        attn_mask = attn_mask.to(x.dtype) if attn_mask is not None else None
-        return self.attn(x, x, x, need_weights=False, attn_mask=attn_mask)[0]
-
-    def forward(self, x: torch.Tensor, attn_mask: Optional[torch.Tensor] = None):
-        x = x + self.ln_attn(self.attention(self.ln_1(x), attn_mask=attn_mask))
-        x = x + self.mlp(self.ln_2(x))
-        return x
 
 
-class Transformer(nn.Module):
+class Transformer(_Transformer):
     def __init__(
         self,
         width: int,
         layers: int,
         heads: int,
         mlp_ratio: float = 4.0,
+        ls_init_value: float = None,
         act_layer: Callable = nn.GELU,
         norm_layer: Callable = LayerNorm,
-        dtype='fp32',
+        dtype=torch.float32,
     ):
-        super().__init__()
-        self.width = width
-        self.layers = layers
-        self.grad_checkpointing = False
+        super().__init__(
+            width, layers, heads, mlp_ratio, ls_init_value, act_layer, norm_layer
+        )
 
         self.resblocks = nn.ModuleList(
             [
@@ -98,6 +77,7 @@ class Transformer(nn.Module):
                     width,
                     heads,
                     mlp_ratio,
+                    ls_init_value=ls_init_value,
                     act_layer=act_layer,
                     norm_layer=norm_layer,
                     dtype=dtype,
@@ -106,105 +86,8 @@ class Transformer(nn.Module):
             ]
         )
 
-    def get_cast_dtype(self) -> torch.dtype:
-        return self.resblocks[0].mlp.c_fc.weight.dtype
 
-    def forward(self, x: torch.Tensor, attn_mask: Optional[torch.Tensor] = None):
-        for r in self.resblocks:
-            if self.grad_checkpointing and not torch.jit.is_scripting():
-                x = checkpoint(r, x, attn_mask)
-            else:
-                x = r(x, attn_mask=attn_mask)
-        return x
-
-
-class TextTransformer(nn.Module):
-    def __init__(
-        self,
-        context_length: int = 77,
-        vocab_size: int = 49408,
-        width: int = 512,
-        heads: int = 8,
-        layers: int = 12,
-        output_dim: int = 512,
-        act_layer: Callable = nn.GELU,
-        norm_layer: Callable = LayerNorm,
-        dtype='fp32',
-    ):
-        super().__init__()
-        self.context_length = context_length
-        self.vocab_size = vocab_size
-        self.width = width
-        self.output_dim = output_dim
-
-        self.token_embedding = nn.Embedding(vocab_size, width)
-        self.positional_embedding = nn.Parameter(
-            torch.empty(self.context_length, width)
-        )
-        self.transformer = Transformer(
-            width=width,
-            layers=layers,
-            heads=heads,
-            act_layer=act_layer,
-            norm_layer=norm_layer,
-            dtype=dtype,
-        )
-        self.ln_final = norm_layer(width)
-        self.text_projection = nn.Parameter(torch.empty(width, output_dim))
-
-        self.register_buffer('attn_mask', self.build_attention_mask(), persistent=False)
-
-        self.init_parameters()
-
-    def init_parameters(self):
-        nn.init.normal_(self.token_embedding.weight, std=0.02)
-        nn.init.normal_(self.positional_embedding, std=0.01)
-
-        proj_std = (self.transformer.width**-0.5) * (
-            (2 * self.transformer.layers) ** -0.5
-        )
-        attn_std = self.transformer.width**-0.5
-        fc_std = (2 * self.transformer.width) ** -0.5
-        for block in self.transformer.resblocks:
-            nn.init.normal_(block.attn.in_proj_weight, std=attn_std)
-            nn.init.normal_(block.attn.out_proj.weight, std=proj_std)
-            nn.init.normal_(block.mlp.c_fc.weight, std=fc_std)
-            nn.init.normal_(block.mlp.c_proj.weight, std=proj_std)
-
-        if self.text_projection is not None:
-            nn.init.normal_(self.text_projection, std=self.transformer.width**-0.5)
-
-    @torch.jit.ignore
-    def set_grad_checkpointing(self, enable=True):
-        self.transformer.grad_checkpointing = enable
-
-    def build_attention_mask(self):
-        # lazily create causal attention mask, with full attention between the vision tokens
-        # pytorch uses additive attention mask; fill with -inf
-        mask = torch.empty(self.context_length, self.context_length)
-        mask.fill_(float("-inf"))
-        mask.triu_(1)  # zero out the lower diagonal
-        return mask
-
-    def forward(self, text):
-        cast_dtype = self.transformer.get_cast_dtype()
-
-        x = self.token_embedding(text).to(cast_dtype)  # [batch_size, n_ctx, d_model]
-
-        x = x + self.positional_embedding.to(cast_dtype)
-        x = x.permute(1, 0, 2)  # NLD -> LND
-        x = self.transformer(x, attn_mask=self.attn_mask)
-        x = x.permute(1, 0, 2)  # LND -> NLD
-        x = self.ln_final(x)
-
-        # x.shape = [batch_size, n_ctx, transformer.width]
-        # take features from the eot embedding (eot_token is the highest number in each sequence)
-        x = x[torch.arange(x.shape[0]), text.argmax(dim=-1)] @ self.text_projection
-
-        return x
-
-
-class VisionTransformer(nn.Module):
+class VisionTransformer(_VisionTransformer):
     def __init__(
         self,
         image_size: int,
@@ -213,103 +96,69 @@ class VisionTransformer(nn.Module):
         layers: int,
         heads: int,
         mlp_ratio: float,
+        ls_init_value: float = None,
         output_dim: int = 512,
         act_layer: Callable = nn.GELU,
         norm_layer: Callable = LayerNorm,
-        dtype='fp32',
+        dtype=torch.float32,
     ):
-        super().__init__()
-        self.image_size = to_2tuple(image_size)
-        self.patch_size = to_2tuple(patch_size)
-        self.grid_size = (
-            self.image_size[0] // self.patch_size[0],
-            self.image_size[1] // self.patch_size[1],
+        super().__init__(
+            image_size,
+            patch_size,
+            width,
+            layers,
+            heads,
+            mlp_ratio,
+            ls_init_value,
+            output_dim,
+            act_layer,
+            norm_layer,
         )
-        self.output_dim = output_dim
-        self.conv1 = nn.Conv2d(
-            in_channels=3,
-            out_channels=width,
-            kernel_size=patch_size,
-            stride=patch_size,
-            bias=False,
-        )
-
-        scale = width**-0.5
-        self.class_embedding = nn.Parameter(scale * torch.randn(width))
-        self.positional_embedding = nn.Parameter(
-            scale * torch.randn(self.grid_size[0] * self.grid_size[1] + 1, width)
-        )
-        self.ln_pre = norm_layer(width)
         self.transformer = Transformer(
             width,
             layers,
             heads,
             mlp_ratio,
+            ls_init_value=ls_init_value,
             act_layer=act_layer,
             norm_layer=norm_layer,
             dtype=dtype,
         )
 
-        self.ln_post = norm_layer(width)
-        self.proj = nn.Parameter(scale * torch.randn(width, output_dim))
 
+class TextTransformer(_TextTransformer):
+    def __init__(
+        self,
+        context_length: int = 77,
+        vocab_size: int = 49408,
+        width: int = 512,
+        heads: int = 8,
+        layers: int = 12,
+        ls_init_value: float = None,
+        output_dim: int = 512,
+        act_layer: Callable = nn.GELU,
+        norm_layer: Callable = LayerNorm,
+        dtype=torch.float32,
+    ):
+        super().__init__(
+            context_length,
+            vocab_size,
+            width,
+            heads,
+            layers,
+            ls_init_value,
+            output_dim,
+            act_layer,
+            norm_layer,
+        )
+
+        self.transformer = Transformer(
+            width=width,
+            layers=layers,
+            heads=heads,
+            ls_init_value=ls_init_value,
+            act_layer=act_layer,
+            norm_layer=norm_layer,
+            dtype=dtype,
+        )
         self.init_parameters()
-
-    def lock(self, unlocked_groups=0, freeze_bn_stats=False):
-        assert (
-            unlocked_groups == 0
-        ), 'partial locking not currently supported for this model'
-        for param in self.parameters():
-            param.requires_grad = False
-
-    def init_parameters(self):
-        # FIXME OpenAI CLIP did not define an init for the VisualTransformer
-        # TODO experiment if default PyTorch init, below, or alternate init is best.
-
-        # nn.init.normal_(self.class_embedding, std=self.scale)
-        # nn.init.normal_(self.positional_embedding, std=self.scale)
-        #
-        # proj_std = (self.transformer.width ** -0.5) * ((2 * self.transformer.layers) ** -0.5)
-        # attn_std = self.transformer.width ** -0.5
-        # fc_std = (2 * self.transformer.width) ** -0.5
-        # for block in self.transformer.resblocks:
-        #     nn.init.normal_(block.attn.in_proj_weight, std=attn_std)
-        #     nn.init.normal_(block.attn.out_proj.weight, std=proj_std)
-        #     nn.init.normal_(block.mlp.c_fc.weight, std=fc_std)
-        #     nn.init.normal_(block.mlp.c_proj.weight, std=proj_std)
-        #
-        # if self.text_projection is not None:
-        #     nn.init.normal_(self.text_projection, std=self.scale)
-        pass
-
-    @torch.jit.ignore
-    def set_grad_checkpointing(self, enable=True):
-        self.transformer.grad_checkpointing = enable
-
-    def forward(self, x: torch.Tensor):
-        x = self.conv1(x)  # shape = [*, width, grid, grid]
-        x = x.reshape(x.shape[0], x.shape[1], -1)  # shape = [*, width, grid ** 2]
-        x = x.permute(0, 2, 1)  # shape = [*, grid ** 2, width]
-        x = torch.cat(
-            [
-                self.class_embedding.to(x.dtype)
-                + torch.zeros(
-                    x.shape[0], 1, x.shape[-1], dtype=x.dtype, device=x.device
-                ),
-                x,
-            ],
-            dim=1,
-        )  # shape = [*, grid ** 2 + 1, width]
-        x = x + self.positional_embedding.to(x.dtype)
-        x = self.ln_pre(x)
-
-        x = x.permute(1, 0, 2)  # NLD -> LND
-        x = self.transformer(x)
-        x = x.permute(1, 0, 2)  # LND -> NLD
-
-        x = self.ln_post(x[:, 0, :])
-
-        if self.proj is not None:
-            x = x @ self.proj
-
-        return x
