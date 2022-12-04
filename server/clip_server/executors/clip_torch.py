6191813870e52bbacbab9bc6f77f4f5262d90f94
YangXiuyu <gzzyyxy@gmail.com>
2022-12-04 22:33:19 +0800

    fix: device

--- server/clip_server/executors/clip_torch.py
+++ server/clip_server/executors/clip_torch.py
@@ -58,7 +58,7 @@ class CLIPEncoder(Executor):
         else:
             self._device = device
         if dtype is None:
-            dtype = 'fp32' if device in ('cpu', torch.device('cpu')) else 'fp16'
+            dtype = 'fp32' if self._device in ('cpu', torch.device('cpu')) else 'fp16'
         self.dtype = dtype
 
         if not self._device.startswith('cuda') and (
