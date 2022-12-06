b559109c1be9b2c755be49be5b74ca4c5b1e7a58
YangXiuyu <gzzyyxy@gmail.com>
2022-12-06 15:17:31 +0800

    fix: ci and get test path

--- scripts/get-all-test-paths.sh
+++ scripts/get-all-test-paths.sh
@@ -6,7 +6,7 @@ BATCH_SIZE=3
 #declare -a array1=( "tests/unit/test_*.py" )
 #declare -a array2=( $(ls -d tests/unit/*/ | grep -v '__pycache__' | grep -v 'array') )
 #declare -a array3=( "tests/unit/array/*.py" )
-declare -a mixins=( $(find tests -name "test_*.py" | grep -v 'test_tensorrt.py') )
+declare -a mixins=( $(find tests -name "test_*.py" | grep -v 'test_tensorrt.py' | grep -v 'test_fp16.py') )
 declare -a array4=( "$(echo "${mixins[@]}" | xargs -n$BATCH_SIZE)" )
 # array5 is currently empty because in the array/ directory, mixins is the only directory
 # but add the following in case new directories are created in array/
