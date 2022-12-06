d47d2c9c9bbfcc9e8a8b2943daa6fbc52ce163d1
YangXiuyu <gzzyyxy@gmail.com>
2022-12-06 19:40:21 +0800

    fix: remove plain text in trt_test

--- tests/test_fp16.py
+++ tests/test_fp16.py
@@ -67,28 +67,6 @@ def test_docarray_inputs(make_flow, inputs):
 @pytest.mark.parametrize(
     'inputs',
     [
-        ['hello, world', 'goodbye, world'],
-        ('hello, world', 'goodbye, world'),
-        lambda: ('hello, world' for _ in range(10)),
-        [
-            'https://docarray.jina.ai/_static/favicon.png',
-            f'{os.path.dirname(os.path.abspath(__file__))}/img/00000.jpg',
-            'hello, world',
-        ],
-    ],
-)
-def test_trt_plain_inputs(make_trt_flow_fp16, inputs):
-    c = Client(server=f'grpc://0.0.0.0:{make_trt_flow_fp16.port}')
-    r = c.encode(inputs if not callable(inputs) else inputs())
-    assert (
-        r.shape[0] == len(list(inputs)) if not callable(inputs) else len(list(inputs()))
-    )
-
-
-@pytest.mark.gpu
-@pytest.mark.parametrize(
-    'inputs',
-    [
         [Document(text='hello, world'), Document(text='goodbye, world')],
         DocumentArray([Document(text='hello, world'), Document(text='goodbye, world')]),
         lambda: (Document(text='hello, world') for _ in range(10)),
