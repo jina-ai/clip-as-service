9c7809149f7348ade41de12dbe417b1ae9a97507
ZiniuYu <ziniuyu@gmail.com>
2023-04-19 11:07:30 +0800

    fix: fix docarray at v1

--- server/setup.py
+++ server/setup.py
@@ -46,6 +46,7 @@ setup(
         'regex',
         'torchvision<=0.13.0' if sys.version_info <= (3, 7, 2) else 'torchvision',
         'jina>=3.12.0',
+        'docarray<0.30.0'
         'prometheus-client',
         'open_clip_torch>=2.8.0',
     ],
