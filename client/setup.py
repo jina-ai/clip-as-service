47cbb2878fe304f901b8e1127c2eb3a9c2182d1d
Zihao Jing <1971008968@qq.com>
2023-12-19 21:56:20 +0800

    docs: update jina version
    jina==3.14.1

--- client/setup.py
+++ client/setup.py
@@ -41,7 +41,7 @@ setup(
     long_description_content_type='text/markdown',
     zip_safe=False,
     setup_requires=['setuptools>=18.0', 'wheel'],
-    install_requires=['jina>=3.12.0', 'docarray[common]>=0.19.0,<0.30.0', 'packaging'],
+    install_requires=['jina==3.14.1', 'docarray[common]>=0.19.0,<0.30.0', 'packaging'],
     extras_require={
         'test': [
             'pytest',
--- server/setup.py
+++ server/setup.py
@@ -45,8 +45,8 @@ setup(
         'torch',
         'regex',
         'torchvision<=0.13.0' if sys.version_info <= (3, 7, 2) else 'torchvision',
-        'jina>=3.12.0',
-        'docarray<0.30.0',
+        'jina==3.14.1',
+        'docarray==0.21.0',
         'prometheus-client',
         'open_clip_torch>=2.8.0',
         'pillow-avif-plugin'
